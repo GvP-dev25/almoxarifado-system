01b32008ed4c4afda3b5e949c5d690b9771febc6
Gabriel Vinicius <gabriel.vpoliveira2@senacsp.edu.br>
2026-08-26 20:15:36 -0300

    test: add quantity and movement tests for BranchProduct

---
+++ test/br/com/almoxarifado/model/BranchProductTest.java
@@ -0,0 +1,93 @@
+package br.com.almoxarifado.model;
+
+import br.com.almoxarifado.exception.InvalidQuantityException;
+import br.com.almoxarifado.exception.InsufficientStockException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BranchProductTest {
+    @Test
+    void addQuantity() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        branchProduct.addQuantity(50, OriginType.INITIALSTOCK, "123");
+        assertEquals(150, branchProduct.getQuantity());
+    }
+
+    @Test
+    void addQuantityException() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        assertThrows(InvalidQuantityException.class, () -> {
+            branchProduct.addQuantity(0, OriginType.INITIALSTOCK, "123");
+        });
+    }
+
+    @Test
+    void addQuantityNegative() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        assertThrows(InvalidQuantityException.class, () -> {
+            branchProduct.addQuantity(-50, OriginType.INITIALSTOCK, "123");
+        });
+    }
+
+
+    @Test
+    void removeQuantity() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        branchProduct.removeQuantity(70, OriginType.INITIALSTOCK, "123");
+        assertEquals(30, branchProduct.getQuantity());
+    }
+
+    @Test
+    void removeZeroQuantity() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        assertThrows(InvalidQuantityException.class, () -> {
+            branchProduct.removeQuantity(0, OriginType.INITIALSTOCK, "123");
+        });
+    }
+
+    @Test
+    void removeQuantityExceedsStock() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        assertThrows(InsufficientStockException.class, () -> {
+            branchProduct.removeQuantity(150, OriginType.INITIALSTOCK, "123");
+        });
+    }
+
+    @Test
+    void movementListSize() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        branchProduct.addQuantity(50, OriginType.INITIALSTOCK, "123");
+        assertEquals(2, branchProduct.getMovementList().size());
+        assertEquals(100, branchProduct.getMovementList().get(0).getQuantity());
+        assertEquals(50, branchProduct.getMovementList().get(1).getQuantity());
+    }
+
+    @Test
+    void movimentsResults() {
+        Product newProduct = new Product("1", "Parafuso 1/2 x 1");
+        Branch branchSouth = new Branch("001", "Branch South");
+        BranchProduct branchProduct = new BranchProduct(newProduct, branchSouth, 100);
+        branchProduct.addQuantity(50, OriginType.INITIALSTOCK, "123");
+        assertEquals(OriginType.INITIALSTOCK, branchProduct.getMovementList().get(1).getOriginType());
+        assertEquals("123", branchProduct.getMovementList().get(1).getOriginNumber());
+    }
+
+
+
+}
