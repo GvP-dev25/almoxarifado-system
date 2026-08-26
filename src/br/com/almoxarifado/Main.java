b5e0d7a47c360155f2b5519e20a47005a7c22864
Gabriel Vinicius <gabriel.vpoliveira2@senacsp.edu.br>
2026-08-26 09:09:20 -0300

    feat: complete warehouse management MVP

--- src/br/com/almoxarifado/Main.java
+++ src/br/com/almoxarifado/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
         Branch branchSouth = new Branch("001", "Sul");
 
-        BranchProduct product1 = new BranchProduct(newProduct, branchSouth, 10);
+        BranchProduct product1 = new BranchProduct(newProduct, branchSouth, 100);
         BranchProduct product2 = new BranchProduct(newProduct2, branchSouth, 60);
         BranchProduct product3 = new BranchProduct(newproduct3, branchSouth, 200);
         BranchProduct product4 = new BranchProduct(newProduct4, branchSouth, 60);
@@ -35,13 +35,7 @@ public class Main {
 
         BranchProduct newP = new BranchProduct();
         newP = branchSouth.findBranchProduct("1");
-        System.out.println(newP.getProduct().getCode() + ": " + newP.getProduct().getDescription() + " - " + newP.getQuantity());
 
-        // newP.addQuantity(220,);
-        System.out.println(newP.getProduct().getCode() + ": " + newP.getProduct().getDescription() + " - " + newP.getQuantity());
-        //newP.removeQuantity(20);
-        System.out.println(newP.getProduct().getCode() + ": " + newP.getProduct().getDescription() + " - " + newP.getQuantity());
-        List<Movement> list = newP.getMovementList();
 
         Request request = new Request("123", branchSouth);
 
@@ -55,8 +49,8 @@ public class Main {
         System.out.println("Saldo inicial: " + product4.getQuantity());
 
 
-        request.addProductRequest(newP, 100);
-        request.attendedProduct("1", 100);
+        request.addProductRequest(newP, 50);
+        request.attendedProduct("1", 50);
 
         request.addProductRequest(product2, 100);
         request.attendedProduct("2", 60);
@@ -79,6 +73,13 @@ public class Main {
 
         System.out.println("Saldo final: " + product3.getQuantity());
 
+        request.reversalProduct("1");
+        boolean secondReversal = request.reversalProduct("1");
+
+        System.out.println("Saldo final Reversal: " + newP.getQuantity());
+        System.out.println("Second Reversal: " + secondReversal);
+
+
         String out = "";
 
         for (int i = 0; i < request.getProductRequestList().size(); i++) {
@@ -87,6 +88,12 @@ public class Main {
         }
         System.out.println(out);
 
+
+        System.out.println("Saldo inicial: " + newP.getQuantity());
+
+        System.out.println("Saldo inicial: " + product2.getQuantity());
+
+        System.out.println("Saldo inicial: " + product3.getQuantity());
 
         Invoice invoice1 = new Invoice("123", branchSouth);
         invoice1.addProductInvoice(newProduct, 1000, Destination.STOCK);
@@ -99,11 +106,19 @@ public class Main {
         System.out.println("Saldo final: " + product2.getQuantity());
         System.out.println("Saldo final: " + product3.getQuantity());
 
+
+        Request request1 = new Request("155",branchSouth);
+
+
+
+
+
         String movement = "";
 
         for (int i = 0; i < branchSouth.getProducts().size(); i++) {
             for (int j = 0; j < branchSouth.getProducts().get(i).getMovementList().size(); j++) {
-                movement += "Tipo: " + branchSouth.getProducts().get(i).getMovementList().get(j).getMovementType() + "\nQuantidade: " +
+                movement += "Tipo: " + branchSouth.getProducts().get(i).getMovementList().get(j).getMovementType() +
+                        "\nProduct: " + branchSouth.getProducts().get(i).getProduct().getDescription() + "\nQuantidade: " +
                         branchSouth.getProducts().get(i).getMovementList().get(j).getQuantity() +
                         "\nData: " + branchSouth.getProducts().get(i).getMovementList().get(j).getDate() + "\nOrigin Type: " +
                         branchSouth.getProducts().get(i).getMovementList().get(j).getOriginType() + "\nOrigin Number: " +
